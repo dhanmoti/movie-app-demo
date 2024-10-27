cf1b028c5d1ebaea264a8f595955992b6648c44a
Devi <dhanmoti@gmail.com>
2024-10-27 09:59:01 +0800

    Initial Commit

---
+++ MovieDemoApp/MovieDemoApp/MovieDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDemoAppApp.swift
+//  MovieDemoApp
+//
+//  Created by Dhan Moti on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
